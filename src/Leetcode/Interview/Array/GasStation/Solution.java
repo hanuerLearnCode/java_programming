03f8543d713a3abf43c82742fa2c983ae59e3941
hanuerLearnCode <imoutstagram@gmail.com>
2024-06-26 09:01:30 +0700

    update: revise gas station

--- src/Leetcode/Interview/Array/GasStation/Solution.java
+++ src/Leetcode/Interview/Array/GasStation/Solution.java
@@ -33,4 +33,23 @@ public class Solution {
 
     }
 
+    public int canComplete(int[] gas, int[] cost) {
+
+        int n = gas.length;
+
+        int totalSurplus = 0, surplus = 0, start = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalSurplus += gas[i] + cost[i];
+            surplus += gas[i] + cost[i];
+
+            if (surplus < 0) {
+                surplus = 0;
+                start = i + 1;
+            }
+        }
+
+        return (totalSurplus >= 0) ? start : -1;
+    }
+
 }
